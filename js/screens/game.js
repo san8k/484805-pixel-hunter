0f3bbe329dd5a13d1d967290e66fcf83d315b9df
Alexander <alrttv@gmail.com>
2018-12-07 19:42:16 +0300

    Вынес повторяющиеся методы в отдельные функции

--- js/screens/game.js
+++ js/screens/game.js
@@ -14,28 +14,36 @@ export const showGame = (questions, index, state) => {
     return index + 1;
   };
 
+  const getTrueAnswer = () => {
+    state.answersList.push(data.results.correct[0]);
+    if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
+      changeScreen(showStats(state));
+    } else {
+      state.level += 1;
+      changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
+    }
+  };
+
+  const getFalseAnswer = () => {
+    state.answersList.push(data.results.wrong);
+    if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
+      changeScreen(showStats(state));
+    } else {
+      state.level += 1;
+      state.lives -= 1;
+      changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
+    }
+  };
+
   if (index === 0) {
     const gameScreen = new gameView.GuessForEach(questions, index, state);
 
     gameScreen.onChangeTrueAnswer = () => {
-      state.answersList.push(data.results.correct[0]);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getTrueAnswer();
     };
 
     gameScreen.onChangeFalseAnswer = () => {
-      state.answersList.push(data.results.wrong);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        state.lives -= 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getFalseAnswer();
     };
 
     gameScreen.onCLickBack = () => {
@@ -48,24 +56,11 @@ export const showGame = (questions, index, state) => {
     const gameScreen = new gameView.GuessForOne(questions, index, state);
 
     gameScreen.onChangeTrueAnswer = () => {
-      state.answersList.push(data.results.correct[0]);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getTrueAnswer();
     };
 
     gameScreen.onChangeFalseAnswer = () => {
-      state.answersList.push(data.results.wrong);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        state.lives -= 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getFalseAnswer();
     };
 
     gameScreen.onCLickBack = () => {
@@ -78,24 +73,11 @@ export const showGame = (questions, index, state) => {
     const gameScreen = new gameView.FindPainting(questions, index, state);
 
     gameScreen.onChangeTrueAnswer = () => {
-      state.answersList.push(data.results.correct[0]);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getTrueAnswer();
     };
 
     gameScreen.onChangeFalseAnswer = () => {
-      state.answersList.push(data.results.wrong);
-      if (state[`lives`] === data.LIVES.death || state[`level`] === data.MAX_LEVEL || state[`answersList`].length === data.MAX_LEVEL) {
-        changeScreen(showStats(state));
-      } else {
-        state.level += 1;
-        state.lives -= 1;
-        changeScreen(showGame(questions, getNextIndex(), Object.assign({}, state)));
-      }
+      getFalseAnswer();
     };
 
     gameScreen.onCLickBack = () => {
