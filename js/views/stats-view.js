b7e30d6981b8013af5d09c39beb796cac14624da
Alexander <alrttv@gmail.com>
2018-12-16 22:41:29 +0300

    Задание 12

--- js/views/stats-view.js
+++ js/views/stats-view.js
@@ -1,5 +1,5 @@
 import AbstractView from '../abstract-view';
-import * as data from '../data/game-data';
+import * as gameData from '../data/game-data';
 import {showResults} from '../util';
 
 export default class StatsView extends AbstractView {
@@ -8,17 +8,49 @@ export default class StatsView extends AbstractView {
     this.results = results;
   }
   get template() {
-    const correctAnswersCount = this.results[this.results.length - 1].answers.filter((it) => it === `correct`).length;
-    const fastAnswersCount = this.results[this.results.length - 1].answers.filter((it) => it === `fast`).length;
-    const slowAnswersCount = this.results[this.results.length - 1].answers.filter((it) => it === `slow`).length;
-    const gameScore = data.calculateScore(this.results[this.results.length - 1].answers, this.results[this.results.length - 1].lives);
+
     const getGameResult = () => {
-      if (gameScore !== -1) {
-        return `Победа!`;
-      } else {
+      if (this.results[this.results.length - 1].answers.length < gameData.ANSWERS_NUMBER) {
         return `Поражение :(`;
+      } else {
+        return `Победа!`;
       }
     };
+
+    let resultsTemplates = [];
+    this.results.forEach((result, i) => {
+      if (result.lives && result.answers) {
+        resultsTemplates.push(result.lives >= 0 ? this.getWinTemplate(result, i + 1) : this.getFailTemplate(result, i + 1));
+      } else {
+        resultsTemplates.push(this.getGameDataErrorTemplate(i + 1));
+      }
+    });
+
+    return `
+    <section class="result">
+    <h2 class="result__title">${getGameResult()}</h2>
+    ${resultsTemplates.join(``)}
+    </section>
+    `;
+  }
+
+  getGameDataErrorTemplate(number) {
+    return `
+    <table class="result__table">
+    <tr>
+      <td class="result__number">${number}.</td>
+      <td>Ошибка данных игры</td>
+    </tr>
+    </table>
+    `;
+  }
+
+  getWinTemplate(result, number) {
+    const correctAnswersCount = result.answers.filter((it) => it === `correct`).length;
+    const fastAnswersCount = result.answers.filter((it) => it === `fast`).length;
+    const slowAnswersCount = result.answers.filter((it) => it === `slow`).length;
+    const gameScore = gameData.calculateScore(result.answers, result.lives);
+
     const getResultTotal = () => {
       if (gameScore !== -1) {
         return gameScore;
@@ -36,86 +68,66 @@ export default class StatsView extends AbstractView {
     };
 
     const getLives = () => {
-      if (this.results[this.results.length - 1].lives < 0) {
+      if (result.lives < 0) {
         return 0;
       }
-      return this.results[this.results.length - 1].lives;
+      return result.lives;
     };
 
     return `
-    <section class="result">
-    <h2 class="result__title">${getGameResult()}</h2>
+    <table class="result__table">
+    <tr>
+      <td class="result__number">${number}.</td>
+      <td colspan="2">
+        <ul class="stats">
+          ${showResults(result.answers)}
+        </ul>
+      </td>
+      <td class="result__points">× 100</td>
+      <td class="result__total">${getAnswersScore()}</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td class="result__extra">Бонус за скорость:</td>
+      <td class="result__extra">${fastAnswersCount} <span class="stats__result stats__result--fast"></span></td>
+      <td class="result__points">× 50</td>
+      <td class="result__total">${fastAnswersCount * gameData.POINTS.fast}</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td class="result__extra">Бонус за жизни:</td>
+      <td class="result__extra">${getLives()} <span class="stats__result stats__result--alive"></span></td>
+      <td class="result__points">× 50</td>
+      <td class="result__total">${getLives() * gameData.POINTS.lifeBonus}</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td class="result__extra">Штраф за медлительность:</td>
+      <td class="result__extra">${slowAnswersCount} <span class="stats__result stats__result--slow"></span></td>
+      <td class="result__points">× 50</td>
+      <td class="result__total">${slowAnswersCount * gameData.POINTS.slow}</td>
+    </tr>
+    <tr>
+      <td colspan="5" class="result__total  result__total--final">${getResultTotal()}</td>
+    </tr>
+  </table>
+    `;
+  }
+
+  getFailTemplate(result, number) {
+    return `
     <table class="result__table">
       <tr>
-        <td class="result__number">1.</td>
-        <td colspan="2">
-          <ul class="stats">
-            ${showResults(this.results[this.results.length - 1].answers)}
-          </ul>
-        </td>
-        <td class="result__points">× 100</td>
-        <td class="result__total">${getAnswersScore()}</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за скорость:</td>
-        <td class="result__extra">${fastAnswersCount} <span class="stats__result stats__result--fast"></span></td>
-        <td class="result__points">× 50</td>
-        <td class="result__total">${fastAnswersCount * data.POINTS.fast}</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">${getLives()} <span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">× 50</td>
-        <td class="result__total">${getLives() * data.POINTS.lifeBonus}</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Штраф за медлительность:</td>
-        <td class="result__extra">${slowAnswersCount} <span class="stats__result stats__result--slow"></span></td>
-        <td class="result__points">× 50</td>
-        <td class="result__total">${slowAnswersCount * data.POINTS.slow}</td>
-      </tr>
-      <tr>
-        <td colspan="5" class="result__total  result__total--final">${getResultTotal()}</td>
-      </tr>
-    </table>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">2.</td>
+        <td class="result__number">${number}.</td>
         <td>
           <ul class="stats">
-            ${showResults(``)}
+            ${showResults(result.answers)}
           </ul>
         </td>
         <td class="result__total"></td>
         <td class="result__total  result__total--final">fail</td>
       </tr>
     </table>
-    <table class="result__table">
-      <tr>
-        <td class="result__number">3.</td>
-        <td colspan="2">
-          <ul class="stats">
-          ${showResults(``)}
-          </ul>
-        </td>
-        <td class="result__points">× 100</td>
-        <td class="result__total">900</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">2 <span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">× 50</td>
-        <td class="result__total">100</td>
-      </tr>
-      <tr>
-        <td colspan="5" class="result__total  result__total--final">950</td>
-      </tr>
-    </table>
-    </section>
     `;
   }
 
